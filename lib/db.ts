1191040b6c17488cc40c6c249d5750242056b139
Dru-429 <ect.dhruv@gmail.com>
2025-03-22 12:21:33 +0530

    Added

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI ;
+const MONGODB_URI = process.env.MONGODB_URI! ;
 
 if (!MONGODB_URI) {
     throw new Error("pls define mongoDB uri in env file")
@@ -16,5 +16,23 @@ export async function connectToDatabase() {
     if(cached.conn) {
         return cached.conn
     }
-    
+    if(!cached.promise) {
+        const opts = {
+            bufferCommands: true,
+            maxPoolSize: 5,
+        }
+        
+        cached.promise = mongoose
+            .connect(MONGODB_URI, opts)
+            .then( () => mongoose.connection );
+    }
+
+    try {
+        cached.conn = await cached.promise
+    } catch (error) {
+        cached.promise = null
+        throw new Error("Check DB")
+    }
+
+    return cached.conn;
 }

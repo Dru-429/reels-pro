a8308dcea2426ea87ef13fc8cb81931cb76e774d
Dru-429 <ect.dhruv@gmail.com>
2025-03-22 13:25:22 +0530

    Added User Schema

--- models/User.ts
+++ models/User.ts
@@ -1,1 +1,33 @@
-     
+import mongoose, { model, models, Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+
+
+export interface IUser {
+    email: string;
+    password: string;
+    comparePassword: (password: string) => Promise<boolean>;
+    _id?: mongoose.Types.ObjectId;
+    createdAt?: Date;
+    updateedAt?: Date;
+}
+
+const userSchema = new Schema<IUser>(
+    {
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+    },
+    {
+        timestamps: true
+    }
+)
+
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10)
+    }
+    next()
+});
+
+const User = models?.User || model<IUser>("User", userSchema);
+
+export default User;
